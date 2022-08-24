f73602d0d5b7c9f8100162ebbef41f594c65e2b8
syed8197 <105443392+syed8197@users.noreply.github.com>
2022-08-24 15:54:21 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp1/ValRefTypes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ValRefTypes
+    {
+
+        public static void Main()
+        {
+            int i = 100;
+            int j;
+            j = 1;
+            Console.WriteLine(j);
+            i++;
+            Console.WriteLine(j);
+
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2;
+            arr2 = arr1;
+
+            arr1[0]++;
+
+            Console.WriteLine(arr2[0]);
+            Console.WriteLine(arr1[0]);
+
+        }
+    }
+}
